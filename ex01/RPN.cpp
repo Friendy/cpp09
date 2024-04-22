00e9a4138a12517d594342dc542ccc3952bb7389
mrubina <friendy.m@gmail.com>
2024-04-23 01:58:38 +0200

    ex01 finished, tests aded

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -1,9 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrubina <mrubina@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/22 23:16:49 by mrubina           #+#    #+#             */
+/*   Updated: 2024/04/23 01:51:25 by mrubina          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "RPN.hpp"
 
 /*CONSTRUCTORS*/
-RPN::RPN()
-{
-}
+RPN::RPN(){}
 
 //Assignment operator:
 RPN &RPN::operator=(RPN const &original)
@@ -20,9 +30,14 @@ RPN::RPN(RPN const &original)
 
 /*FUNCTIONS*/
 
-static bool isoperator(int n)
+bool RPN::isoperator(int n)
 {
 	return(n == '+' || n == '-' || n == '*'|| n == '/');
+}
+
+bool RPN::is_digit(int n)
+{
+	return(n >= '0' && n <= '9');
 }
 
 
@@ -42,6 +57,12 @@ int RPN::operation(int operand1, int operand2, int token)
 	return(0);
 }
 
+int RPN::err()
+{
+	std::cerr << "Error\n";
+	exit(0);
+}
+
 int RPN::runStack(std::string arg)
 {
 	int operand1;
@@ -51,20 +72,29 @@ int RPN::runStack(std::string arg)
 	{
 		if (*it != ' ')
 		{
-			if (!isoperator(*it))
+			if (!is_digit(*it) && !isoperator(*it))
+				err();
+			if (is_digit(*it))
 				_stack.push(*it - '0');
-			else
+			else if (!_stack.empty())
 			{
 				operand2 = _stack.top();
 				_stack.pop();
-				operand1 = _stack.top();
-				_stack.pop();
-				operand1 = this->operation(operand1, operand2, *it);
-				if (it != arg.end())
-					_stack.push(operand1);
+				if (!_stack.empty())
+				{
+					operand1 = _stack.top();
+					_stack.pop();
+					operand1 = this->operation(operand1, operand2, *it);
+					if (it != arg.end() - 1)
+						_stack.push(operand1);
+				}
+				else
+					err();
 			}
 		}
 	}
+	if (!_stack.empty())
+		err();
 	return(operand1);
 }
 
--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrubina <mrubina@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/22 23:16:52 by mrubina           #+#    #+#             */
+/*   Updated: 2024/04/23 01:45:19 by mrubina          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef RPN_HPP
 # define RPN_HPP
 # include <string>
@@ -12,6 +24,9 @@ private:
 public:
 	int runStack(std::string arg);
 	int operation(int operand1, int operand2, int token);
+	int err();
+	bool is_digit(int n);
+	bool isoperator(int n);
 
 	RPN();
 	RPN(RPN const &original);
